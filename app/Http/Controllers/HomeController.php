6b98e81601facbd923f63de0d62a3c1ea951189a
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2022-09-14 15:44:38 +0600

    first

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Doctor;
+use App\Models\Appointment;
 
 class HomeController extends Controller
 {
@@ -15,7 +17,8 @@ class HomeController extends Controller
         {
             if(Auth::user()->usertype=='0')
             {
-                return view('user.home');
+                $doctor = doctor::all();
+                return view('user.home', compact('doctor'));
             }
             else{
                 return view('admin.home');
@@ -28,7 +31,35 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('user.home');
+        if(Auth::id())
+        {
+            return $this->redirect('home');
+        }
+        else{
+            $doctor = doctor::all();
+            return view('user.home', compact('doctor'));
+        }
+
+    }
+
+    public function appointment(Request $request)
+    {
+        $data = new appointment;
+
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->date = $request->date;
+        $data->phone = $request->phone;
+        $data->message = $request->message;
+        $data->doctor = $request->doctor;
+        $data->status = 'In progress';
+        if(Auth::id())
+        {
+            $data->user_id = Auth::user()->id;
+        }
+        $data->save();
+        return redirect()->back();
+
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,3 +29,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/add_doctor_view', [AdminController::class, 'addview']);
+Route::post('/upload_doctor', [AdminController::class, 'upload']);
+Route::post('/appointment', [HomeController::class, 'appointment']);
